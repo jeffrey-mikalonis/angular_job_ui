8fece5a5ef511329c05a9e9a38b7ca59e87aa76b
Jeffrey Mikalonis <jeffrey.mikalonis@protonmail.com>
2021-03-07 02:38:37 -0500

    with error

---
+++ src/app/jobs-list/jobs-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+export interface Job {
+  name: string;
+}
+
+@Component({
+  selector: 'jobs-list',
+  templateUrl: './jobs-list.component.html',
+  styleUrls: ['./jobs-list.component.css']
+})
+export class JobsListComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  jobs: Job[] = [];
+
+  ngOnInit() {
+    this.http.get(`{environment.apiUrl}/jobs`).subscribe((resp: any) => {
+      this.jobs = resp;
+    })
+  }
+
+}
